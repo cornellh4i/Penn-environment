9b783be9a67eab6d7610259abab7c50b7ef9dcce
charusm18 <charusm18@gmail.com>
2022-02-03 12:36:45 -0500

    backend

--- myreactapp/src/carousalComponent.js
+++ myreactapp/src/carousalComponent.js
@@ -1,9 +1,8 @@
-import React from "react";
-import Carousel from "react-elastic-carousel";
-import Item from "./Item";
-import "./style.css";
-import { Link } from "react-router-dom";
-
+import React from 'react';
+import Carousel from 'react-elastic-carousel';
+import Item from './Item';
+import './style.css';
+import { Link } from 'react-router-dom';
 
 const breakPoints = [
   { width: 1, itemsToShow: 1 },
@@ -13,90 +12,113 @@ const breakPoints = [
 ];
 
 class CarouselComponent extends React.Component {
-
   constructor(props) {
     super(props);
-    console.log(this.props.featured)
+    console.log(this.props.featured);
     this.state = { data: [] };
-    fetch("http://localhost:3001/showall")
-      .then(data => data.json())
-      .then(data => {
+    fetch('/showall')
+      .then((data) => data.json())
+      .then((data) => {
         this.setState({ data: data });
       });
   }
 
-
-
   render() {
     return (
-      <div className="App">
-        <Carousel breakPoints={breakPoints} style={{marginBottom: "3%"}}>
+      <div className='App'>
+        <Carousel breakPoints={breakPoints} style={{ marginBottom: '3%' }}>
           {this.state.data.map((stateInfo, index) => {
-            console.log(stateInfo.featured)
-            if(this.props.featured && stateInfo.featured)
-              {return <Item key = {index}>
-                <Link to={{
-                  pathname: "/bill",
-                  state: { data: stateInfo },
-                }}>
-                  <div id = "billCard" className="billCard">
+            console.log(stateInfo.featured);
+            if (this.props.featured && stateInfo.featured) {
+              return (
+                <Item key={index}>
+                  <Link
+                    to={{
+                      pathname: '/bill',
+                      state: { data: stateInfo },
+                    }}
+                  >
+                    <div id='billCard' className='billCard'>
+                      <div className='bill' id='wrapper'>
+                        <div id='billInfo'>
+                          <h2 className='senateBillText'>
+                            Senate Bill {stateInfo.bill_number}
+                          </h2>
+                          <h2 className='senatorName'>
+                            Senator {stateInfo.bill_sponsor}
+                          </h2>
+                          <h2 className='introduced'>
+                            Introduced on {stateInfo.bill_intro_date}
+                          </h2>
+                          <p className='referred'>
+                            {' '}
+                            Referred to as {stateInfo.bill_name}
+                          </p>
+                        </div>
 
-                    <div className="bill" id = "wrapper">
-                      <div id = "billInfo">
-                      <h2 className="senateBillText">Senate Bill {stateInfo.bill_number}</h2>
-                      <h2 className="senatorName">Senator {stateInfo.bill_sponsor}</h2>
-                      <h2 className="introduced">Introduced on {stateInfo.bill_intro_date}</h2>
-                      <p className="referred"> Referred to as {stateInfo.bill_name}</p>
+                        <div id='wrapper'>
+                          <p className='text'>
+                            <div class='center'>
+                              <u>Bill Summary</u> <br />
+                              <br />
+                              {stateInfo.bill_summary}
+                            </div>
+                          </p>
+                        </div>
                       </div>
-
-                      <div id = "wrapper">   
-                        <p className="text"><div class= "center"><u>Bill Summary</u> <br/><br/>{stateInfo.bill_summary}</div></p>
-                      </div>
-                    
-                  </div>
-                    <div className="billImg">
+                      <div className='billImg'></div>
                     </div>
-                  </div>
+                  </Link>
+                </Item>
+              );
+            } else if (!this.props.featured) {
+              return (
+                <Item key={index}>
+                  <Link
+                    to={{
+                      pathname: '/bill',
+                      state: { data: stateInfo },
+                    }}
+                  >
+                    <div id='billCard' className='billCard'>
+                      <div className='bill' id='wrapper'>
+                        <div id='billInfo'>
+                          <h2 className='senateBillText'>
+                            Senate Bill {stateInfo.bill_number}
+                          </h2>
+                          <h2 className='senatorName'>
+                            Senator {stateInfo.bill_sponsor}
+                          </h2>
+                          <h2 className='introduced'>
+                            Introduced on {stateInfo.bill_intro_date}
+                          </h2>
+                          <p className='referred'>
+                            {' '}
+                            Referred to as {stateInfo.bill_name}
+                          </p>
+                        </div>
 
-
-                </Link>
-              </Item>}
-               else if(!this.props.featured)         
-               {return <Item key = {index}>
-               <Link to={{
-                 pathname: "/bill",
-                 state: { data: stateInfo },
-               }}>
-                 <div id = "billCard" className="billCard">
-
-                   <div className="bill" id = "wrapper">
-                     <div id = "billInfo">
-                     <h2 className="senateBillText">Senate Bill {stateInfo.bill_number}</h2>
-                     <h2 className="senatorName">Senator {stateInfo.bill_sponsor}</h2>
-                     <h2 className="introduced">Introduced on {stateInfo.bill_intro_date}</h2>
-                     <p className="referred"> Referred to as {stateInfo.bill_name}</p>
-                     </div>
-
-                     <div id = "wrapper">                      
-                      <p className="text"><div class= "center"><u>Bill Summary</u> <br/><br/>{stateInfo.bill_summary}</div></p>
-                     </div>
-                   
-                 </div>
-                   <div className="billImg">
-                   </div>
-                 </div>
-
-
-               </Link>
-             </Item>}
-        })}
-
+                        <div id='wrapper'>
+                          <p className='text'>
+                            <div class='center'>
+                              <u>Bill Summary</u> <br />
+                              <br />
+                              {stateInfo.bill_summary}
+                            </div>
+                          </p>
+                        </div>
+                      </div>
+                      <div className='billImg'></div>
+                    </div>
+                  </Link>
+                </Item>
+              );
+            }
+          })}
         </Carousel>
       </div>
     );
   }
 }
-
-
 
 export default CarouselComponent;
